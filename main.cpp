eb531a1e1be160ebbf266750b12f7b43def9756e
DARWICH ALI <adarwich@etu.unistra.fr>
2022-01-30 00:45:57 +0100

    cpp testing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-
+#include <vector>
 
 
 int main(int argc, char const *argv[]) {
     (void)argc;
     (void)argv;
-    std::cout << "Hello_world" << std::endl;
+    std::vector<int> v{1,2,3};
+    for (const int& i : v)
+    {
+        std::cout << i << " " ;
+    }
+
+    std::cout << std::endl ;
+
     return 0;
 }
