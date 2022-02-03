ee530c7e2a705840667c928961da181a225a729f
DARWICH ALI <adarwich@etu.unistra.fr>
2022-02-03 01:05:29 +0100

    tests

--- run.sh
+++ run.sh
@@ -8,9 +8,14 @@ if [ $# = 0 ]; then
     mkdir bin/
     mv ./build/Tempest bin/
     exit 0
-fi
-if [[ $1 == "clean" ]]; then
-        rm -r build
-        rm -r bin
+
+elif [[ $1 == "clean" ]]; then
+    rm -r build
+    rm -r bin
+elif [[ $1 == "test" ]]; then
+    echo "begin Testing"
+    # lance sh qui lance des test unit && intégration
+    sh test.sh
+    echo "Done testing"
 fi
 exit 0
