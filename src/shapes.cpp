f9e0103b8b5ce7f8b2d966abd73369cf4d647c06
DARWICH ALI <adarwich@etu.unistra.fr>
2022-02-20 01:09:44 +0100

    futur

---
+++ src/shapes.cpp
@@ -0,0 +1,28 @@
+
+
+// 1. Circle
+// 2. Square
+// 3. Plus symbol
+// 4. Bow-tie
+// 5. Stylized Cross
+// 6. Triangle
+// 7. Clover
+// 8. V
+// 9. Steps
+// 10. U
+// 11. Completely Flat
+// 12. Heart
+// 13. Star
+// 14. W
+// 15. Fan
+// 16. Infinity Symbol (figure 8 on its side)
+
+
+shapes::shapes()
+{
+
+}
+
+// crée grille
+// créer weopons
+// monster
